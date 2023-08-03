448b97f9a046d2c36c66c156d7260b07dd131b8a
RonaldDeW <83015544+RonaldDeW@users.noreply.github.com>
2023-08-03 09:28:06 +0200

    Update CashCardApplication.java

--- src/main/java/example/cashcard/CashCardApplication.java
+++ src/main/java/example/cashcard/CashCardApplication.java
@@ -1,3 +1,7 @@
+package example.cashcard;
+
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
